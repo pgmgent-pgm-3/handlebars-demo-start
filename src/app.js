f3dd8caa446436976f8411bec5ff014bde4fa166
Frederick Roegiers <frederick@rogerthat.be>
2023-02-19 09:28:37 +0100

    update 2023

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import express from "express";
-import  "dotenv/config";
-import * as path from "path";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve('src', 'views', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("src", "views", "index.html"));
 });
 
 app.listen(process.env.PORT, () => {
   console.log(`To Do application is listening to port ${process.env.PORT}.`);
-})
+});
